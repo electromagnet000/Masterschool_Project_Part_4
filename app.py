ee848d5b966995715bb8ab76e1b443f40925b419
electromagnet000 <sergiuhowie@campus.masterschool.com>
2023-07-06 00:28:17 +0100

    I updated the stylesheet to asthetically to better improve the UE UX for the users. I add a 404 html in case certain errors are encountered

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 app.secret_key = "super_secret"
 data_manager = json_data_manager("dataManagement/json_data.json")
 
+
 """
 The list of users page
 """
-
 @app.route("/", methods=["GET", "POST"])
 def list_users():
     users = data_manager.list_all_users()
@@ -19,10 +19,10 @@ def list_users():
     return render_template("users.html", users=users)
 
 
-"""
-The Movie choices of the user
-"""
 
+"""
+The Movie Database of the user
+"""
 @app.route('/users/<int:user_id>')
 def user_movies(user_id):
     user = [name for name in data_manager.list_all_users() if name["id"] == user_id]
@@ -31,10 +31,10 @@ def user_movies(user_id):
     return render_template("user_page.html", user_id=user_id, user=user, movies=the_movies)
 
 
+
 """
 Adds a new user
 """
-
 @app.route('/add_user', methods=["GET", "POST"])
 def add_user():
     if request.method == "POST":
@@ -45,10 +45,10 @@ def add_user():
     return render_template("add_user.html")
 
 
+
 """
 Adds a movie to the users database
 """
-
 @app.route('/users/<int:user_id>/add_movie', methods=["GET", "POST"])
 def add_movie(user_id):
     user_movie = data_manager.get_user_movies(user_id)
@@ -62,6 +62,7 @@ def add_movie(user_id):
 
         return render_template("add_movie.html", movies=new_movie_list, id=user_id, success_message=success_message)
     return render_template("add_movie.html", movies=user_movie, id=user_id)
+
 
 
 """
@@ -83,15 +84,20 @@ def update_movie(user_id, movie_id):
 Deletes a film from the users database
 """
 
+
 @app.route("/users/<user_id>/delete_movie/<movie_id>", methods=["POST"])
 def delete_movie(user_id, movie_id):
     movie_to_delete = request.form.get("delete_movie")
     success_message = data_manager.delete_movie(user_id, movie_id, movie_to_delete)
     return redirect(url_for("user_movies", user_id=user_id, success_message=success_message))
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+@app.route("/something")
+def something():
+    return render_template("something.html")
 
 if __name__ in "__main__":
     app.run(debug=True)
