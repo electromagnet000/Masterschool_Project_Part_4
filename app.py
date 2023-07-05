74ce7edc78d7eec3f40da613ac4b1c95633618f1
electromagnet000 <sergiuhowie@campus.masterschool.com>
2023-07-05 16:25:32 +0100

    The brand new changes, added a delete function and updated the update function

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
 from functionality.Data_managment import json_data_manager, CSVDataManager
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 """
 I like to assign global variables at the top for ease of use
 """
 app = Flask(__name__)
-
+app.secret_key = "super_secret"
 data_manager = json_data_manager("dataManagement/json_data.json")
 
+"""
+The list of users page
+"""
 
-@app.route('/')
-def home():
-    return "welcome user"
 
-
-@app.route("/users")
+@app.route("/", methods=["GET", "POST"])
 def list_users():
     users = data_manager.list_all_users()
+
     return render_template("users.html", users=users)
+
+
+"""
+The Movie choices of the user
+"""
 
 
 @app.route('/users/<int:user_id>')
 def user_movies(user_id):
+    user = [name for name in data_manager.list_all_users() if name["id"] == user_id]
     the_movies = data_manager.get_user_movies(user_id)
-    return str(the_movies)
+    return render_template("user_page.html", user_id=user_id, user=user, movies=the_movies)
+
+
+"""
+Adds a new user
+"""
 
 
 @app.route('/add_user', methods=["GET", "POST"])
@@ -36,19 +47,43 @@ def add_user():
     return render_template("add_user.html")
 
 
-@app.route('/users/<user_id>/add_movie')
-def add_movie():
-    pass
+"""
+Adds a movie to the users database
+"""
 
 
-@app.route('/users/<user_id>/update_movie/<movie_id>')
-def update_movie():
-    pass
+@app.route('/users/<int:user_id>/add_movie', methods=["GET", "POST"])
+def add_movie(user_id):
+    user_movie = data_manager.get_user_movies(user_id)
+
+    if request.method == "POST":
+        movie_title = request.form.get("movie_name")
+        movie_notes = request.form.get("movie_notes")
+
+        success_message = data_manager.add_movie(user_id, movie_title, movie_notes)
+        new_movie_list = data_manager.get_user_movies(user_id)
+
+        return render_template("add_movie.html", movies=new_movie_list, id=user_id, success_message=success_message)
+    return render_template("add_movie.html", movies=user_movie, id=user_id)
 
 
-@app.route("/users/<user_id>/delete_movie/<movie_id>")
-def delete_movie():
-    pass
+@app.route('/users/<user_id>/update_movie/<movie_id>', methods=["GET", "POST"])
+def update_movie(user_id, movie_id):
+    if request.method == ["POST"]:
+        note_updates = request.form.get("notes")
+        print(note_updates)
+        data_manager.update_user_movie(user_id, movie_id, note_updates)
+
+        return redirect(url_for("user_movie", user_id=user_id))
+
+    return render_template("update.html", id=user_id, movie_id=movie_id)
+
+
+@app.route("/users/<user_id>/delete_movie/<movie_id>", methods=["DELETE"])
+def delete_movie(user_id, movie_id):
+    movie_to_delete = request.form.get("delete_movie")
+    print(data_manager.delete_movie(user_id, movie_id, movie_to_delete))
+    return redirect(url_for("user_movies", user_id=user_id))
 
 
 if __name__ in "__main__":
--- functionality/Data_managment.py
+++ functionality/Data_managment.py
@@ -2,6 +2,7 @@ from .Data_interface import Data_manager
 import json
 import csv
 import pandas as pd
+import requests
 
 
 # handles all the json datafiles
@@ -34,36 +35,34 @@ class json_data_manager(Data_manager):
         users_id = [user["id"] for user in self.data]
 
         # I like to zip the information and turn it into a dictionary for a clear relationship between the id/name.
-        users = dict(zip(users_id, users_names))
+        users = [dict(zip(users_id, users_names))]
 
         # Lastly I return the information
         return users
 
     """
-    returns a list of all users and their data
+    Returns a list of all users and their data from the json file
     """
 
     def list_all_users(self):
         # Using list comprehension im able to extract specific information from the json data
         users = [user for user in self.data]
-
         # Lastly I return the information
         return users
 
     """
-    creates a brand new user
+    Creates a brand new user
     """
 
     def add_user(self, new_username):
-        users = self.list_all_users()
         new_id = self.generate_id()
 
         new_user = {"id": new_id, "name": new_username, "movies": []}
 
-        users.append(new_user)
+        self.data.append(new_user)
 
         with open(self.filename, "w") as new_file:
-            json.dump(users, new_file)
+            json.dump(self.data, new_file)
             print(f"Successfully added {new_username}")
 
     """
@@ -82,51 +81,120 @@ class json_data_manager(Data_manager):
             # If there are no movies found then it returns a message saying nothing was found
             if not user_movies:
                 user = self.data[user_id - 1]
-                return f"No movies found for user : {user['name']}"
+                return [f"No movies found for user : {user['name']}"]
 
             return user_movies
         except ValueError as e:
             print("User ID not found")
 
-    def update_user_movie(self, user_id):
+    """
+    Update Function : offers users a way to update the film ratings
+    """
 
-        user_data = self.data
-        movies = self.get_user_movies(user_id)
-
-        """
-        Usually the user would type the title of the film to update them 
-        however I dyslexia and I want to minimise the error chance, so 
-        I would rather have people type the id number or the title whichever makes them 
-        more comfortable
-        """
-        print(movies)
-        chosen_title = input("please either write the 'Id' or 'Name' of the movie you want to update")
+    def update_user_movie(self,user_id, chosen_title, notes):
 
         # now I filter through the movie data for chosen title
-        for sublist in range(len(user_data)):
-            if user_data[sublist]["id"] == user_id:
-                for movie in user_data[sublist]["movies"]:
+        for user in range(len(self.data)):
+            if self.data[user]["id"] == user_id:
+                for movie in self.data[user]["movies"]:
                     # here I filter through the chosen users movies with a simple if statement
-                    if str(movie["id"]) == chosen_title or movie["name"].lower() == chosen_title.lower():
-
-                        # if the movie is found I would like the user to see visual confirmation of the movie
-                        print(movie)
-
-                        # human error handling to make sure that the rating is between 0 - 10
-                        while True:
-                            new_rating = float(input("What is the new rating you want the movie to have : "))
-                            if new_rating > 10 or new_rating < 0:
-                                print("please enter a rating between 0 - 10")
-                                continue
-                            break
-
+                    if movie["id"] == chosen_title:
                         # now we assign the new rating and return a confirmation message
-                        movie["rating"] = new_rating
+                        movie["Notes"] = notes
 
                 # lastly we store the updated data
                 with open(self.filename, "w") as new_file:
-                    json.dump(user_data, new_file)
+                    json.dump(self.data, new_file)
                     return "success"
+
+    """
+    Add movie function
+    """
+
+    def add_movie(self, user_id, title, user_notes):
+
+        # using my api key I can get access to the OMDB api
+        API_KEY = "ae79a6f6"
+        # sends a get request to gather specific information from Movies API
+        api_url = "http://www.omdbapi.com/?t={}&apikey={}".format(title, API_KEY)
+        response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+        # if the response status is 200 then it proceeds with the request
+        if response.status_code == requests.codes.ok:
+            # creates a variable for the text repsonse
+
+            fetched_data = response.json()
+
+            # creates an empty dict with an id number for the film
+            requested_data = {}
+
+            movies = [movie for sublist in self.data if sublist['id'] == user_id for movie in sublist["movies"]]
+
+            requested_data['id'] = len(movies) + 1
+
+            # creates the parameters we need to get
+            requested_parameters = ['Title', 'Year', 'imdbRating', 'Poster', 'imdbID']
+
+            # iterates through the keys to get the correct parameters and stores them in requested_data
+            for key, value in fetched_data.items():
+                if key in requested_parameters:
+                    requested_data[key] = value
+
+            requested_data["Notes"] = user_notes
+
+            # checks if anything was actually fetched
+            try:
+                if not requested_data['Title']:
+                    return f"Movie {title} not found in API"
+            except KeyError as e:
+                return f"Movie {title} not found in API database"
+
+            for data in range(len(self.data)):
+                if self.data[data]["id"] == user_id:
+                    self.data[data]["movies"].append(requested_data)
+
+            # creates a new file with all the added information
+            with open(self.filename, "w") as new_file:
+                json.dump(self.data, new_file)
+                return f"Movie {title} successfully added"
+
+    """
+    A function to Delete movies
+    """
+    def delete_movie(self, user_id, movie_id, title):
+
+        # Using a for loop we can filter information until we get the desired info
+        for user in range(len(self.data)):
+            # here we check if the user id is correct
+            if self.data[user]["id"] == int(user_id):
+                # here we loop over each film and check if the parameters are correct such as title and film id
+                for movie in self.data[user]["movies"]:
+                    if movie["Title"] == title and movie["id"] == int(movie_id):
+                        # If the parameters are correct then delete the film
+                        self.data[user]["movies"].remove(movie)
+
+        """
+        Now theres a potential issue, when you delete a film the total films will reduce by 1 each time you use this 
+        function this will cause problems when adding new films when generating their id. Because the generate a film Id
+        uses len("movies) + 1 this will cause problems
+        """
+
+        """
+        My solution is to make a for loop that updates the film Id when the delete function activated
+        """
+
+        updated_id = 1
+
+        for user in range(len(self.data)):
+            if self.data[user]["id"] == int(user_id):
+                for movie in self.data[user]["movies"]:
+                    movie["id"] = updated_id
+                    updated_id += 1
+        """
+        Now the film IDs are unique 
+        """
+
+        with open(self.filename, "w") as new_file:
+            json.dump(self.data, new_file)
 
 
 # here is a csv data manager
@@ -157,7 +225,7 @@ class CSVDataManager(Data_manager):
     A function that gets the users information such as Name and ID number
     """
 
-    def get_all_users(self):
+    def csv_get_all_users(self):
         # opening the filedata
         with open(self.filename, "r") as data:
             # using pandas to create a readable structure
@@ -181,7 +249,7 @@ class CSVDataManager(Data_manager):
     Returns a list of dictionaries containing the films
     """
 
-    def get_user_movies(self, user_id):
+    def csv_get_user_movies(self, user_id):
         # opens the csv
         with open(self.filename, "r") as data:
             df = csv.DictReader(data)
@@ -198,10 +266,14 @@ class CSVDataManager(Data_manager):
             except Exception as e:
                 return f"I'm sorry there seems to be an error {e}"
 
-    def update_user_movie(self, user_id, movie_name):
+    def csv_update_user_movie(self, user_id, movie_name):
         pass
 
-    def delete_movie(self, user_id, title):
+    def csvdelete_movie(self, user_id, title):
 
         movies = self.get_user_movies(user_id)
         return movies
+
+
+
+
